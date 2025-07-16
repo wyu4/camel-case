173aa4261e0b67b480edc5d02c462514a198e88b
wyu4 <wilsonyu657@gmail.com>
2025-07-16 16:57:29 -0400

    Create placeholder website
    Make cababas undraggable
    
    Update README.md
    
    Move contents from index.css to App.css
    
    Create Cababas-themed placeholder page
    
    Add scrappy cababas for hahas
    
    Remove period
    
    This triggered me
    
    Upscale and convert to ico
    
    Upscale and convert to ico

--- src/Components/CababasEmblem.tsx
+++
@@ -1,9 +0,0 @@
-import cababasImage from "../assets/Cababas.png";
-
-const CababasEmblem = () => {
-    return <img src={cababasImage} className="cababas"></img>
-};
-
-CababasEmblem.displayName = "cababas-emblem";
-
-export default CababasEmblem;
