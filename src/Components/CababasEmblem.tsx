bcbcdb7c1678cc6649d3320dfca7f9dd7376bd07
wyu4 <wilsonyu657@gmail.com>
2025-07-16 17:02:15 -0400

    Make cababas undraggable

--- src/Components/CababasEmblem.tsx
+++ src/Components/CababasEmblem.tsx
@@ -1,7 +1,7 @@
 import cababasImage from "../assets/Cababas.png";
 
 const CababasEmblem = () => {
-    return <img src={cababasImage} className="cababas"></img>
+    return <img src={cababasImage} className="cababas" draggable={false}></img>
 };
 
 CababasEmblem.displayName = "cababas-emblem";
