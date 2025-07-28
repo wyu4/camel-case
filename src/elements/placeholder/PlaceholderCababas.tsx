a88d9f601b4bb09a1c625f78859b224c5c43da3a
Wilson <wilsonyu657@gmail.com>
2025-07-27 23:21:19 -0400

    Change elements to functions and move images to public

---
+++ src/elements/placeholder/PlaceholderCababas.tsx
@@ -0,0 +1,11 @@
+import cababasImage from "/Cababas.webp";
+
+export default function PlaceholderCababas() {
+    return (
+        <img
+            src={cababasImage}
+            className="cababas placeholder"
+            draggable={false}
+        ></img>
+    );
+};
