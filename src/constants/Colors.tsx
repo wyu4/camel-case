f0d9ae0054bc2a3387987d0c9a6d7775e039310e
wyu4 <wilsonyu657@gmail.com>
2025-08-23 00:02:12 -0400

    Add color palette as constants

---
+++ src/constants/Colors.tsx
@@ -0,0 +1,7 @@
+export const MAIN_BLUE = "#cae2d8";
+export const MAIN_GREEN = "#287a5c";
+export const MAIN_BROWN = "#ca9459";
+
+export const HIGH_BROWN = "#905b41";
+export const HIGH_DARK_BROWN = "#4f4d39";
+export const HIGH_BEIGE = "#f4e5c9";
