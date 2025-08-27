a4966bea1516645bd05c204761b80065004c4ea7
wyu4 <wilsonyu657@gmail.com>
2025-08-27 19:35:38 -0400

    Remove unecessary indents

--- src/elements/TopBar.tsx
+++ src/elements/TopBar.tsx
@@ -1,6 +1,8 @@
 export default function TopBar() {
-    return <div className="top-bar">
-        <button>Foo</button>
-        <button>Bar</button>
-    </div>
+    return (
+        <div className="top-bar">
+            <button>Foo</button>
+            <button>Bar</button>
+        </div>
+    );
 }
