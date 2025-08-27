9dbffe14cf662fcced98784da27350fef5af495b
wyu4 <wilsonyu657@gmail.com>
2025-08-27 17:58:49 -0400

    Seperate production and placeholder into different pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const root = resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    root,
+    plugins: [react()],
+    server: {
+        port: 3001,
+    },
+    build: {
+        outDir: resolve(__dirname, "dist"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, "index.html"),
+                placeholder: resolve(
+                    root,
+                    "pages",
+                    "placeholder",
+                    "index.html"
+                ),
+            },
+        },
+    },
+});
