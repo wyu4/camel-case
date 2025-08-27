2a33c11b47925509715e6eaf3e88b44fa1376955
wyu4 <wilsonyu657@gmail.com>
2025-08-27 18:34:08 -0400

    Move placeholder to src folder
    Not super satisfied about this, but it is what it is.

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
                 main: resolve(root, "index.html"),
                 placeholder: resolve(
                     root,
-                    "pages",
                     "placeholder",
                     "index.html"
                 ),
