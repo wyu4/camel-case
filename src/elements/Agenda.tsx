f5c00e758d2f79e737921c7a74997b0ffe295472
wyu4 <wilsonyu657@gmail.com>
2025-08-30 00:33:52 -0400

    Create fully working schedule UI

--- src/elements/Agenda.tsx
+++ src/elements/Agenda.tsx
@@ -1,20 +1,30 @@
 import { useEffect, useRef, useState, type JSX } from "react";
 import Plack from "./Plack";
+import { wrapNum } from "../global/NumberHelpers";
 
 function EventSchedule({ schedule }: { schedule: ScheduleProps }) {
-    const dayLabel = useRef<HTMLHeadingElement>(null);
     const [tabs, setTabs] = useState<JSX.Element[]>([]);
+    const [dayNumber, setDayNumber] = useState(0);
+    const eventsContainer = useRef<HTMLDivElement>(null);
 
-    const onLastButtonClick = () => {};
-    const onNextButtonClick = () => {};
+    const onLastButtonClick = () => {
+        setDayNumber((last) => wrapNum(last - 1, 0, tabs.length));
+    };
+    const onNextButtonClick = () => {
+        setDayNumber((last) => wrapNum(last + 1, 0, tabs.length));
+    };
 
     useEffect(() => {
         if (schedule.length > 0) {
-            const createEvent = ({ time, name }: EventProps, index: number) => {
+            const createEvent = (
+                { time, name }: EventProps,
+                eventNumber: number,
+                dayNumber: number
+            ) => {
                 return (
                     <div
                         className="event rounded"
-                        key={`${index}-${time}-${name}`}
+                        key={`${dayNumber}-${eventNumber}-${time}-${name}`}
                     >
                         <p className="time">{time}</p>
                         <p className="name">{name}</p>
@@ -23,31 +33,51 @@ function EventSchedule({ schedule }: { schedule: ScheduleProps }) {
             };
 
             const newTabs: JSX.Element[] = [];
-            const createEventTab = (data: EventProps[]) => {
+            const createEventTab = (data: EventProps[], dayNumber: number) => {
                 const events: JSX.Element[] = [];
                 data.forEach((event, i) => {
-                    events.push(createEvent(event, i));
+                    events.push(createEvent(event, i, dayNumber));
                 });
                 return events;
             };
-            schedule.forEach((day) => {
-                newTabs.push(<div>{createEventTab(day)}</div>);
+            schedule.forEach((daySchedule, i) => {
+                newTabs.push(<div key={`daySchedule-${i}`}>{createEventTab(daySchedule, i)}</div>);
             });
 
             setTabs(newTabs);
         }
     }, [schedule]);
 
+    useEffect(() => {
+        if (eventsContainer.current) {
+            eventsContainer.current.style.transform = `translateX(-${
+                dayNumber * 100
+            }%)`;
+        }
+    }, [dayNumber]);
+
     return (
         <Plack className="schedule">
             <div className="day">
-                <button className="last" onClick={onLastButtonClick}></button>
-                <h3 ref={dayLabel}>
-                    Day 1
-                </h3>
-                <button className="next" onClick={onNextButtonClick}></button>
+                <button
+                    className="last transparent"
+                    onClick={onLastButtonClick}
+                >
+                    {"<"}
+                </button>
+                <h3>{`Day ${dayNumber + 1}`}</h3>
+                <button
+                    className="next transparent"
+                    onClick={onNextButtonClick}
+                >
+                    {">"}
+                </button>
             </div>
-            <div className="events plack darkener rounded">{tabs[0]}</div>
+            <div className="events-container plack darkener rounded">
+                <div className="events transparent" ref={eventsContainer}>
+                    {tabs}
+                </div>
+            </div>
         </Plack>
     );
 }
