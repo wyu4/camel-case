c61751298b1059f1fc2a83dfa37a992fa01aa8ca
Wilson <wilsonyu657@gmail.com>
2025-09-08 14:27:06 -0400

    Create question buttons

--- src/elements/FAQ.tsx
+++ src/elements/FAQ.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 
 type QuestionProps = {
     question: string;
@@ -7,8 +7,9 @@ type QuestionProps = {
 }
 
 function QuestionResponse({question, answer, children=[]} : QuestionProps) {
+    const [opened, setOpened] = useState(false);
     
-    return <div className="question"></div>
+    return (<button className="question plack rounded padded"><p>{question}</p><span className="material-icons">keyboard_arrow_down</span></button>);
 }
 
 export default function FAQ() {
@@ -16,7 +17,11 @@ export default function FAQ() {
     return (
         <section className="faq">
             <h2>FAQ</h2>
-            
+            <div className="container">
+                <QuestionResponse question="Why did the chicken cross the road?" answer="To get to the other side!"/>
+                <QuestionResponse question="How much does this cost?" answer="This costs roughly $1000000 to attend."/>
+                <QuestionResponse question="What should I make?" answer="Anything sweet. Baked goods are recommended."/>
+            </div>
         </section>
     );
 }
