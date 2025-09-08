a72ecd54acc8e03255d3a3a97c02f7df420f989d
Wilson <wilsonyu657@gmail.com>
2025-09-08 13:56:12 -0400

    Install material icons

--- src/elements/FAQ.tsx
+++ src/elements/FAQ.tsx
@@ -1,9 +1,22 @@
+import type { JSX } from "react";
+
+type QuestionProps = {
+    question: string;
+    answer: string;
+    children?: JSX.Element[];
+}
+
+function QuestionResponse({question, answer, children=[]} : QuestionProps) {
+    
+    return <div className="question"></div>
+}
+
 export default function FAQ() {
 
     return (
         <section className="faq">
             <h2>FAQ</h2>
-            <p>Pretty much last designy section, but this one could just be hardcoded.</p>
+            
         </section>
     );
 }
