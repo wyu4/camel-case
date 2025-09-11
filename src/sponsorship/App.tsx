197a0877978e10538e0bc5616e3afd0e7b1fd0a6
Wilson <wilsonyu657@gmail.com>
2025-09-11 13:03:44 -0400

    Optimize sponsorship package viewer

--- src/sponsorship/App.tsx
+++ src/sponsorship/App.tsx
@@ -1,27 +1,3 @@
-import { useEffect, useState } from "react";
-import { getImagePath, SPONSORS_API } from "../global/APIHelpers";
-
 export default function App() {
-    const [pdfLink, setPdfLink ] = useState("");
-
-    useEffect(() => {
-        // Get the sponsors data
-        const getSponsors = async () => {
-            try {
-                const response = await fetch(SPONSORS_API);
-                if (!response.ok) {
-                    throw new Error(`Request code ${response.status}`);
-                }
-                const formattedResult: AllSponsorsProps = await response.json();
-                setPdfLink("https://drive.google.com/viewerng/viewer?embedded=true&url=" + getImagePath(formattedResult.package));
-            } catch (e: unknown) {
-                console.error(`Couldn't get sponsors: ${e}`);
-            }
-        };
-        getSponsors();
-    }, []);
-
-    return <>
-        {(pdfLink !== "") ? <object id="content" data={pdfLink} width="100vw" height="100vh" /> : null}
-    </>;
+    return <object id="content" data={"/documents/SponsorshipPackage.pdf"} width="100vw" height="100vh" />;
 }
