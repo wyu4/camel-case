2718709607c896dc8eda6a034652a27b81a28a85
wyu4 <wilsonyu657@gmail.com>
2025-09-15 17:17:28 -0400

    Use iFrame instead of embed

--- src/sponsorship/App.tsx
+++ src/sponsorship/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-    return <embed src="/documents/SponsorshipPackage.pdf" type="application/pdf" width="100%" height="100%" />;
+    return <iframe src="/documents/SponsorshipPackage.pdf" width="100%" height="100%" />;
 }
