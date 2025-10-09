20e3f75042a30b008ef5bea04d17bc5642ba0460
Wilson <wilsonyu657@gmail.com>
2025-10-09 17:18:41 -0400

    Add missing @ in email

--- src/elements/EmailLink.tsx
+++ src/elements/EmailLink.tsx
@@ -1,3 +1,3 @@
 export default function EmailLink() {
-    return <a href="mailto:camelcasehackathon@gmail.com">camelcasehackathongmail.com</a>
+    return <a href="mailto:camelcasehackathon@gmail.com">camelcasehackathon@gmail.com</a>
 }
