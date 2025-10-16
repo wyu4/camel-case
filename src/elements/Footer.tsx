2194e02bae8b33b5833c0c4375b7383c64289bcf
Wilson <wilsonyu657@gmail.com>
2025-10-16 04:41:09 -0400

    Add signatures to footer

--- src/elements/Footer.tsx
+++ src/elements/Footer.tsx
@@ -1,10 +1,38 @@
+import Alyn from "/images/AlynSignature.webp";
+import Elliott from "/images/ElliottSignature.webp";
+import Erin from "/images/ErinSignature.webp";
+import Fiona from "/images/FionaSignature.webp";
+import Jocelyn from "/images/JocelynSignature.webp";
+import Umaiza from "/images/UmaizaSignature.webp";
+import Wilson from "/images/WilsonSignature.webp";
+
+function Signature({ name, src }: { name: string; src: string }) {
+    return (
+        <div
+            className={"signature " + name}
+            style={{
+                maskImage: `url(${src})`,
+            }}
+        />
+    );
+}
+
 export default function Footer() {
     return (
         <section className="footer">
             <div className="header">
                 <p>Made with</p>
                 <div className="heart" />
-                <p>by highschoolers</p>
+                <p>by</p>
+            </div>
+            <div className="signatures">
+               <Signature name="alyn" src={Alyn} />
+               <Signature name="elliott" src={Elliott} />
+               <Signature name="erin" src={Erin} />
+               <Signature name="fiona" src={Fiona} />
+               <Signature name="jocelyn" src={Jocelyn} />
+               <Signature name="umaiza" src={Umaiza} />
+               <Signature name="wilson" src={Wilson} />
             </div>
         </section>
     );
