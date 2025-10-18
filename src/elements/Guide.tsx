8f3d0f0d255c1ec1456d1785f9b639f028863b7b
Wilson <wilsonyu657@gmail.com>
2025-10-18 19:06:24 -0400

    Rearrange TSX layout to make adding obstacles easier

--- src/elements/Guide.tsx
+++ src/elements/Guide.tsx
@@ -37,10 +37,10 @@ export default function Guide() {
 }
 
 function Background() {
-    const parallax1 = 0.1;
-    const parallax2 = 0.15;
-    const parallax3 = 0.2;
-    const parallax4 = 0.25;
+    const parallax1 = 0;
+    const parallax2 = 1 / 8;
+    const parallax3 = 1 / 7;
+    const parallax4 = 1 / 6;
 
     const backgroundRef = useRef<HTMLDivElement>(null);
     const [sectionY, setSectionY] = useState(0);
@@ -57,57 +57,90 @@ function Background() {
 
     return (
         <div className="background" ref={backgroundRef}>
-            <div className="left">
-                <div
-                    className="layer l1"
-                    style={{
-                        transform: `translateY(${sectionY * parallax1}px)`,
-                    }}
-                />
-                <div
-                    className="layer l2"
-                    style={{
-                        transform: `translateY(${sectionY * parallax2}px)`,
-                    }}
-                />
-                <div
-                    className="layer l3"
-                    style={{
-                        transform: `translateY(${sectionY * parallax3}px)`,
-                    }}
-                />
-                <div
-                    className="layer l4"
-                    style={{
-                        transform: `translateY(${sectionY * parallax4}px)`,
-                    }}
-                />
+            <div
+                className="layer l1"
+                style={{
+                    transform: `translateY(${sectionY * parallax1}px)`,
+                }}
+            >
+                <div className="middle" />
+                <div className="left" />
+                <div className="right" />
             </div>
-            <div className="right">
-                <div
-                    className="layer l1"
-                    style={{
-                        transform: `translateY(${sectionY * parallax1}px)`,
-                    }}
-                />
-                <div
-                    className="layer l2"
-                    style={{
-                        transform: `translateY(${sectionY * parallax2}px)`,
-                    }}
-                />
-                <div
-                    className="layer l3"
-                    style={{
-                        transform: `translateY(${sectionY * parallax3}px)`,
-                    }}
-                />
-                <div
-                    className="layer l4"
-                    style={{
-                        transform: `translateY(${sectionY * parallax4}px)`,
-                    }}
-                />
+            <div
+                className="layer l2"
+                style={{
+                    transform: `translateY(${sectionY * parallax2}px)`,
+                }}
+            >
+                <div className="middle">
+                    <div
+                        className="prop"
+                        style={{
+                            top: "10%",
+                            transform: "rotateZ(10deg)",
+                        }}
+                    />
+                    <div
+                        className="prop"
+                        style={{
+                            top: "70%",
+                            transform: "rotateZ(-5deg)",
+                        }}
+                    />
+                </div>
+                <div className="left" />
+                <div className="right" />
+            </div>
+            <div
+                className="layer l3"
+                style={{
+                    transform: `translateY(${sectionY * parallax3}px)`,
+                }}
+            >
+                <div className="middle">
+                    <div
+                        className="prop"
+                        style={{
+                            top: "20%",
+                            transform: "rotateZ(5deg)",
+                        }}
+                    />
+                    <div
+                        className="prop"
+                        style={{
+                            top: "50%",
+                            transform: "rotateZ(-3deg)",
+                        }}
+                    />
+                </div>
+                <div className="left" />
+                <div className="right" />
+            </div>
+            <div
+                className="layer l4"
+                style={{
+                    transform: `translateY(${sectionY * parallax4}px)`,
+                }}
+            >
+                <div className="middle">
+                    <div
+                        className="prop"
+                        style={{
+                            top: "40%",
+                            transform: "rotateZ(-6deg)",
+                        }}
+                    />
+                    <div
+                        className="prop"
+                        style={{
+                            top: "80%",
+                            transform: "rotateZ(5deg)",
+                        }}
+                    />
+                </div>
+                <div className="left" />
+                <div className="right" />
             </div>
         </div>
     );
