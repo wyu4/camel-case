e601181a557e6882e361c2824151e08fd2883972
Wilson <wilsonyu657@gmail.com>
2025-10-18 16:16:19 -0400

    Start styling signs in guide

--- src/elements/Guide.tsx
+++ src/elements/Guide.tsx
@@ -1,64 +1,114 @@
 import { useEffect, useRef, useState } from "react";
+import Plack from "./Plack";
+import { getImagePath } from "../global/APIHelpers";
+import Line from "./Line";
 
 export default function Guide() {
-    const sectionRef = useRef<HTMLDivElement>(null);
+    return (
+        <section className="guide">
+            <Background />
+            <div className="content">
+                <div className="row">
+                    <Plack style={{ transform: "rotateZ(5deg)" }}>
+                        <div className="header">
+                            <h2>SIGN UP</h2>
+                            <h3> for free!</h3>
+                        </div>
+                        <Line lineColor="var(--high-brown)" />
+                        <button>Sign up form!</button>
+                    </Plack>
+                </div>
+                <div className="row">
+                    <Plack style={{ transform: "rotateZ(-5deg)" }}>
+                        <h2>TEAM UP</h2>
+                        <h3> with other teens!</h3>
+                    </Plack>
+                </div>
+
+                <div className="row">
+                    <Plack style={{ transform: "rotateZ(2deg)" }}>
+                        <h2>BUILD</h2>
+                        <h3> something cool!</h3>
+                    </Plack>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+function Background() {
+    const parallax1 = 0.1;
+    const parallax2 = 0.15;
+    const parallax3 = 0.2;
+    const parallax4 = 0.25;
+
+    const backgroundRef = useRef<HTMLDivElement>(null);
     const [sectionY, setSectionY] = useState(0);
 
     useEffect(() => {
-        if (sectionRef.current) {
+        if (backgroundRef.current) {
             setSectionY(
                 window.screenY +
                     window.innerHeight / 2 -
-                    sectionRef.current.getBoundingClientRect().top
+                    backgroundRef.current.getBoundingClientRect().top
             );
         }
-    }, [window.scrollY, window.innerHeight, sectionRef]);
+    }, [window.scrollY, window.innerHeight, backgroundRef]);
 
     return (
-        <section className="guide" ref={sectionRef}>
-            <div className="background">
-                <div className="left">
-                    <div className="layer l1" />
+        <div className="background" ref={backgroundRef}>
+            <div className="left">
+                <div
+                    className="layer l1"
+                    style={{
+                        transform: `translateY(${sectionY * parallax1}px)`,
+                    }}
+                />
                 <div
                     className="layer l2"
                     style={{
-                        transform: `translateY(${sectionY * 0.05}px)`,
+                        transform: `translateY(${sectionY * parallax2}px)`,
                     }}
                 />
                 <div
                     className="layer l3"
                     style={{
-                        transform: `translateY(${sectionY * 0.1}px)`,
+                        transform: `translateY(${sectionY * parallax3}px)`,
                     }}
                 />
                 <div
                     className="layer l4"
                     style={{
-                        transform: `translateY(${sectionY * 0.15}px)`,
+                        transform: `translateY(${sectionY * parallax4}px)`,
                     }}
                 />
-                </div>
-                <div className="right">
-                    <div className="layer l1" />
-                    <div className="layer l2" />
-                    <div className="layer l3" />
-                    <div className="layer l4" />
-                </div>
             </div>
-            <div className="content">
-                <div style={{ transform: "rotateZ(5deg)" }}>
-                    <h2>SIGN UP</h2>
-                    <h3> for free!</h3>
-                </div>
-                <div style={{ transform: "rotateZ(-5deg)" }}>
-                    <h2>TEAM UP</h2>
-                    <h3> with other teens!</h3>
-                </div>
-                <div style={{ transform: "rotateZ(2deg)" }}>
-                    <h2>BUILD</h2>
-                    <h3> something cool!</h3>
-                </div>
+            <div className="right">
+                <div
+                    className="layer l1"
+                    style={{
+                        transform: `translateY(${sectionY * parallax1}px)`,
+                    }}
+                />
+                <div
+                    className="layer l2"
+                    style={{
+                        transform: `translateY(${sectionY * parallax2}px)`,
+                    }}
+                />
+                <div
+                    className="layer l3"
+                    style={{
+                        transform: `translateY(${sectionY * parallax3}px)`,
+                    }}
+                />
+                <div
+                    className="layer l4"
+                    style={{
+                        transform: `translateY(${sectionY * parallax4}px)`,
+                    }}
+                />
             </div>
-        </section>
+        </div>
     );
 }
