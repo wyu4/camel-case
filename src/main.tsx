d6ac93efe5196b6e7f36afc00110beb87f348a8f
Wilson <wilsonyu657@gmail.com>
2025-11-09 11:44:04 -0500

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "leaflet/dist/leaflet.css";
-import 'material-icons/iconfont/material-icons.css';
-import "material-symbols"
+import "material-icons/iconfont/material-icons.css";
+import "material-symbols";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
